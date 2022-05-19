ad68f412ced0c6dd661319c219b52851ede1a94e
Mox1k <83392087+Mox1k@users.noreply.github.com>
2022-05-19 14:14:53 +0300

    Добавьте файлы проекта.

---
+++ DocumentManagementSystem/Domain/Entites/User.cs
@@ -0,0 +1,50 @@
+﻿using DocumentManagementSystem.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentManagementSystem.Domain.Entites
+{
+    /// <summary>
+    /// Пользователь
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public Guid Id { get; set; }
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string? Name { get; set; }
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        public string? Surname { get; set; }
+        /// <summary>
+        /// Отчество
+        /// </summary>
+        public string? Patronymic { get; set; }
+        /// <summary>
+        /// Должность
+        /// </summary>
+        public Posts Posts { get; set; }
+        /// <summary>
+        /// Код сотрудника
+        /// </summary>
+        public int Code { get; set; }
+        /// <summary>
+        /// Логин
+        /// </summary>
+        public string? Login { get; set; }
+        /// <summary>
+        /// Пароль
+        /// </summary>
+        public string? Password { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+    }
+}
